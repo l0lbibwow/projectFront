fb2d571571646b0f2e038b74845a7045c199e6e0
l0lbibwow <69837266+l0lbibwow@users.noreply.github.com>
2021-01-17 22:06:04 +0600

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { User } from '../shared/user';
 
 @Injectable({
