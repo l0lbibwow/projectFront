fe4062591966d7aef8e31064b17eb9320e3816bf
Bibigul Otepbergenova <bibigul.tulegenovna@mail.ru>
2021-01-25 17:06:13 +0600

    change custom directives

--- src/app/admin-or-user.directive.ts
+++ src/app/admin-or-user.directive.ts
@@ -1,21 +1,26 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { Observable, Subscription, interval  } from 'rxjs';
 
 @Directive({
   selector: '[appAdminOrUser]'
 })
 export class AdminOrUserDirective implements OnInit{
+  private updateSubscription: Subscription;
+  constructor(private elementRef: ElementRef, private aServ: AuthService){}
 
-  constructor(private elementRef: ElementRef, private authService: AuthService) { }
-
-
-  ngOnInit(){
+  ngOnInit(): void{
+    this.updateSubscription = interval(1000).subscribe(
+     (val) => { this.block();
+    });
+    }
+  block(): void{
     const localUsers = localStorage.getItem('token');
-    if ((localUsers === 'admin' || localUsers === 'moder') && this.authService.authUser) {
+    if ((localUsers === 'admin' || localUsers === 'moder') && this.aServ.authUser) {
         this.elementRef.nativeElement.style.display = 'block';
     }else{
         this.elementRef.nativeElement.style.display = 'none';
     }
   }
-}
 
+}
